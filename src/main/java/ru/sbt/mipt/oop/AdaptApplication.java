6778d51b600669e052d095f60dfde7a6d8ba72f7
maksim <polyanskimax@gmail.com>
2019-04-03 19:51:04 +0300

    add alarm decorator to apadtApplication

--- src/main/java/ru/sbt/mipt/oop/AdaptApplication.java
+++ src/main/java/ru/sbt/mipt/oop/AdaptApplication.java
@@ -2,22 +2,28 @@ package ru.sbt.mipt.oop;
 
 import com.coolcompany.smarthome.events.SensorEventsManager;
 import ru.sbt.mipt.oop.coolcompanyadapters.CCHandlerToHandlerAdapter;
-import ru.sbt.mipt.oop.event.handlers.AlarmEventHandler;
-import ru.sbt.mipt.oop.event.handlers.DoorSensorEventHandler;
-import ru.sbt.mipt.oop.event.handlers.LightSensorEventHandler;
+import ru.sbt.mipt.oop.event.handlers.*;
 import ru.sbt.mipt.oop.homeinputoutput.JsonHomeReader;
 
 public class AdaptApplication {
     public static void main(String[] args) {
         SmartHome smartHome = new JsonHomeReader("smart-home-1.js").readSmartHome();
         SensorEventsManager sensorEventsManager = new SensorEventsManager();
-        sensorEventsManager.registerEventHandler(new CCHandlerToHandlerAdapter(
-                new DoorSensorEventHandler(smartHome)));
-        sensorEventsManager.registerEventHandler(new CCHandlerToHandlerAdapter(
-                new LightSensorEventHandler(smartHome)));
-        sensorEventsManager.registerEventHandler(new CCHandlerToHandlerAdapter(
-                new AlarmEventHandler(smartHome)));
-
+        sensorEventsManager.registerEventHandler(
+                new CCHandlerToHandlerAdapter(
+                        new AlarmDecoratorSensorEventHandler(
+                                new DoorSensorEventHandler(smartHome), smartHome.getAlarm())));
+        sensorEventsManager.registerEventHandler(
+                new CCHandlerToHandlerAdapter(
+                        new AlarmDecoratorSensorEventHandler(
+                                new LightSensorEventHandler(smartHome), smartHome.getAlarm())));
+        sensorEventsManager.registerEventHandler(
+                new CCHandlerToHandlerAdapter(
+                        new AlarmDecoratorSensorEventHandler(
+                                new CloseHallDoorEventHandler(smartHome), smartHome.getAlarm())));
+        sensorEventsManager.registerEventHandler(
+                new CCHandlerToHandlerAdapter(
+                        new AlarmEventHandler(smartHome)));
         sensorEventsManager.start();
     }
 }
